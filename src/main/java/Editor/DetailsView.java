b6d53007d7ee57d3d3b70e59ab0ef9f66d74fe9e
19booob92 <19mateusz92@gmail.com>
2014-03-12 20:21:11 +0100

    Dodano obsluge klikniec na elementach listy

--- src/main/java/Editor/DetailsView.java
+++ src/main/java/Editor/DetailsView.java
@@ -1,39 +1,56 @@
 package Editor;
+
 import java.awt.Dimension;
-import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class DetailsView extends JFrame{
-	
+public class DetailsView extends JFrame {
+
 	private JPanel panel;
 	private JTextArea questContent = new JTextArea("Insert quest content");
 	private JButton choosePics = new JButton("Chose files");
 	private JList<String> paths;
+	private JLabel pointNameLbl;
 	
-	public DetailsView() {
+	private String pointName = "default";
+	
+	public DetailsView(String pointName) {
 		super("Details view");
+		
+		this.pointName = pointName;
 		
 		panel = new JPanel();
 		
-		panel.setLayout(new GridLayout());
-		
 		paths = new JList<String>();
+		paths.setBounds(121, 37, 70, 50);
 		paths.setPreferredSize(new Dimension(70, 50));
+		questContent.setBounds(26, 10, 129, 15);
+		pointNameLbl = new JLabel(this.pointName);
 		
 		panel.add(questContent);
+		choosePics.setBounds(160, 5, 112, 25);
 		panel.add(choosePics);
 		panel.add(paths);
+		panel.add(pointNameLbl);
 		
-		add(panel);
+		getContentPane().add(panel);
 		
 		setSize(new Dimension(300, 300));
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setVisible(true);
 		
+	}
+
+	public String getPointName() {
+		return pointName;
+	}
+
+	public void setPointName(String pointName) {
+		this.pointName = pointName;
 	}
 }
--- src/main/java/Editor/MainView.java
+++ src/main/java/Editor/MainView.java
@@ -1,7 +1,6 @@
 package Editor;
 import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,45 +9,61 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
+public class MainView extends JFrame {
 
-public class MainView extends JFrame{
-	
 	private JTextField nameOfGame = new JTextField("Name of quest");
 	private JTextField something = new JTextField("[something]");
-	private JList<MapPoint> list;
-	private JButton listBtn = new JButton("Detals");
+	public JButton listBtn = new JButton("Details");
 	private JPanel panel;
+	private JList list;
+	private String pointName = "default";
 	
-	private MapPoint [] mapPoints = {new MapPoint(), new MapPoint()};
+	private DetailsView detailsView;
+	
+	private String [] mapPoints = {"test 0", "test 1"};
 	
 	public MainView() {
 		super("Editor");
 		panel = new JPanel(); 
-		panel.setLayout(new GridLayout(2,2));
-		list = new JList<MapPoint>(mapPoints);
-		list.setPreferredSize(new Dimension(50,100));
-
-//		Block of listeners
+		listBtn.setBounds(150, 68, 80, 25);
+		list = new JList(mapPoints);
 		
+//		Block of listeners
+		list.addListSelectionListener(new ListSelectionListener() {
+			
+			public void valueChanged(ListSelectionEvent event) {
+				pointName = list.getSelectedValue().toString();
+			}
+		});
+
 		listBtn.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent arg0) {
+
 				EventQueue.invokeLater(new Runnable() {
-					
 					public void run() {
-						DetailsView dv = new DetailsView();
+						detailsView = new DetailsView(pointName);
+						System.out.println(detailsView.getPointName());
 					}
 				});
 			}
 		});
 		
+		panel.setLayout(null);
+		nameOfGame.setBounds(16, 8, 96, 19);
+		
 		panel.add(nameOfGame);
+		something.setBounds(117, 8, 80, 19);
 		panel.add(something);
-		panel.add(list);
 		panel.add(listBtn);
 		
-		add(panel);
+		getContentPane().add(panel);
+		
+		list.setBounds(26, 35, 86, 103);
+		panel.add(list);
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(new Dimension(300, 300));
@@ -62,5 +77,13 @@ public class MainView extends JFrame{
 				new MainView();
 			}
 		});
+	}
+
+	public DetailsView getDetailsView() {
+		return detailsView;
+	}
+
+	public void setDetailsView(DetailsView detailsView) {
+		this.detailsView = detailsView;
 	}
 }
