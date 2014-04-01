34e9ae1fa2aeaa20cc0d6aa57432d32028f9c8ba
19booob92 <19mateusz92@gmail.com>
2014-04-01 13:56:20 +0200

    Dodano tabelke obsluge requestow o wszystkich userow, ale na razie to BETA

---
+++ src/main/java/UserRegistration/UserGameDTO.java
@@ -0,0 +1,38 @@
+package UserRegistration;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.codehaus.jackson.annotate.JsonAutoDetect;
+
+@XmlRootElement
+@JsonAutoDetect
+public class UserGameDTO implements Serializable{
+	
+	private String user;
+	private int points;
+	private Date endTime;
+	
+	
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+}
